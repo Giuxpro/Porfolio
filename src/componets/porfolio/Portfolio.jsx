78fd6e0a6745708accac22f01a26b7948c7584b1
Giuxpro <albertogiusepp@gmail.com>
2022-05-14 03:51:44 -0300

    portfolio and review done

--- src/componets/porfolio/Portfolio.jsx
+++ src/componets/porfolio/Portfolio.jsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import "./Portfolio.css"
+import React from "react";
+import "./Portfolio.css";
+import IMG1 from "../../assets/web1.png";
+import IMG2 from "../../assets/web2.png";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio6.jpg";
+import data from "../../json/data.json";
 
 const Portfolio = () => {
   return (
-    <section id='porfolio'>Portfolio</section>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        {data.map(({ id, title, image, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img className="portfolio__img" src={IMG1} alt="GameWeb" />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a
+                  href={github}
+                  className="btn"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo "Soon"
+                </a>
+              </div>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
---
+++ src/componets/porfolio/porfolioInfo.js
@@ -0,0 +1,95 @@
+<div>
+  <article className="portfolio__item">
+    <div className="portfolio__item-image">
+      <img className="portfolio__img" src={"IMG1"} alt="GameWeb" />
+    </div>
+    <h3>Giux Video Game Web</h3>
+    <div className="portfolio__item-cta">
+      <a
+        href="https://github.com/Giuxpro/PI-VideoGames"
+        className="btn"
+        target="_blank"
+      >
+        Github
+      </a>
+      <a href="#" className="btn btn-primary" target="_blank">
+        Live Demo "Soon"
+      </a>
+    </div>
+  </article>
+
+  <article className="portfolio__item">
+    <div className="portfolio__item-image">
+      <img className="portfolio__img" src={"IMG2"} alt="GameWeb" />
+    </div>
+    <h3>Another proyect soon</h3>
+    <div className="portfolio__item-cta">
+      <a href="#" className="btn" target="_blank">
+        Github
+      </a>
+      <a href="#" className="btn btn-primary" target="_blank">
+        Live Demo
+      </a>
+    </div>
+  </article>
+
+  <article className="portfolio__item">
+    <div className="portfolio__item-image">
+      <img className="portfolio__img" src={"IMG3"} alt="GameWeb" />
+    </div>
+    <h3>Another proyect soon</h3>
+    <div className="portfolio__item-cta">
+      <a href="#" className="btn" target="_blank">
+        Github
+      </a>
+      <a href="#" className="btn btn-primary" target="_blank">
+        Live Demo "Soon"
+      </a>
+    </div>
+  </article>
+
+  <article className="portfolio__item">
+    <div className="portfolio__item-image">
+      <img className="portfolio__img" src={"IMG4"} alt="GameWeb" />
+    </div>
+    <h3>Another proyect soon</h3>
+    <div className="portfolio__item-cta">
+      <a href="#" className="btn" target="_blank">
+        Github
+      </a>
+      <a href="#" className="btn btn-primary" target="_blank">
+        Live Demo "Soon"
+      </a>
+    </div>
+  </article>
+
+  <article className="portfolio__item">
+    <div className="portfolio__item-image">
+      <img className="portfolio__img" src={"IMG4"} alt="GameWeb" />
+    </div>
+    <h3>Another proyect soon</h3>
+    <div className="portfolio__item-cta">
+      <a href="#" className="btn" target="_blank">
+        Github
+      </a>
+      <a href="#" className="btn btn-primary" target="_blank">
+        Live Demo "Soon"
+      </a>
+    </div>
+  </article>
+
+  <article className="portfolio__item">
+    <div className="portfolio__item-image">
+      <img className="portfolio__img" src={"IMG4"} alt="GameWeb" />
+    </div>
+    <h3>Another proyect soon</h3>
+    <div className="portfolio__item-cta">
+      <a href="#" className="btn" target="_blank">
+        Github
+      </a>
+      <a href="#" className="btn btn-primary" target="_blank">
+        Live Demo "Soon"
+      </a>
+    </div>
+  </article>
+</div>;
