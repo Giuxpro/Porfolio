2c9b7052aa0dba78938372684ef5ad43b1d8152c
Giuxpro <albertogiusepp@gmail.com>
2022-05-15 00:08:44 -0300

    website done only a few details less to be done

--- src/componets/contact/Contact.jsx
+++ src/componets/contact/Contact.jsx
@@ -1,10 +1,72 @@
 import React from 'react'
 import "./Contact.css"
+import {MdEmail} from "react-icons/md"
+import {ImLinkedin} from "react-icons/im"
+import {BsGithub} from "react-icons/bs"
+import {ImWhatsapp} from "react-icons/im"
+import  { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-const contact = () => {
+const {PHONE_NUMBER, EMAIL_SERVICE_ID, TEMPLATE_SERVICE_ID, PUBLIC_KEY} = process.env
+
+
+const Contact = () => {
+  const form = useRef();
+  
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm("service_x13b9ju", "template_zdkz01m",  form.current, "M-Qkyci830QPzlOwZ")
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+      alert("Email Send")
+  };
+
   return (
-    <section id='contact'>contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>albertogiusepp@gmail.com</h5>
+            <a href="mailto:albertogiusepp@gmail.com" target="_blank">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <ImLinkedin className='contact__option-icon'/>
+            <h4>Linkedin</h4>
+            <h5>https://www.linkedin.com/in/giu-dev/</h5>
+            <a href="https://www.linkedin.com/in/giu-dev/" target="_blank">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <BsGithub className='contact__option-icon'/>
+            <h4>GitHub</h4>
+            <h5>https://github.com/Giuxpro</h5>
+            <a href="https://github.com/Giuxpro" target="_blank">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <ImWhatsapp/>
+            <h4>WhatsApp</h4>
+            <h5></h5>
+            <a href={`https://api.whatsapp.com/send?phone=922276249`} target="_blank">Send a Message</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full Name' required />
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
-export default contact
+export default Contact
--- src/componets/footer/Footer.jsx
+++ src/componets/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import "./Footer.css"
+import {BsLinkedin} from "react-icons/bs"
+import {IoLogoWhatsapp} from "react-icons/io"
+import {MdEmail} from "react-icons/md"
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Giux</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="https://www.linkedin.com/in/giu-dev/" target="_blank"><BsLinkedin/></a>
+        <a href={`https://api.whatsapp.com/send?phone=922276249`} target="_blank"><IoLogoWhatsapp/></a>
+        <a href="mailto:albertogiusepp@gmail.com" target="_blank"><MdEmail/></a>
+      </div>
+
+      <div className="footer__copyright">
+          <small>&copy; Giux, All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
